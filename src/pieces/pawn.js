11b200551ed2a86315daef4d2945779c6d7eec61
leeroywking <leeroywking@gmail.com>
2019-10-26 16:17:13 -0700

    added pawn constructor hinting

--- src/pieces/pawn.js
+++ src/pieces/pawn.js
@@ -1,8 +1,19 @@
 // 'use strict'
 
-export default class Pawn{
-    constructor(startingSpace, color, board){
-
+export default class Pawn {
+  constructor(startingSpace, color, board) {
+    availableMoves: this.howItMoves(startingSpace) // [move spaces]
+    monitoredSpaces: this.howItMonitors(startingSpace) // [monitored spaces]
+    currentSpace: startingSpace
+  }
+  name = 'Pawn'
+  howItMoves = (startingSpace, color) => {
+    if (color === 'black') {
+      // starting space 
+      return startingSpace /*board[i][j]*/
     }
-    name = 'Pawn'
+  }
+  howItMonitors = (startingSpace) => {
+    return monitoredSpaces
+  }
 } 
