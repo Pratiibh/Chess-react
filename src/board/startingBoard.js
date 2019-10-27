c79ec51c54dd73856221c0e45591f32c09a016f9
leeroywking <leeroywking@gmail.com>
2019-10-26 18:28:06 -0700

    .

--- src/board/startingBoard.js
+++ src/board/startingBoard.js
@@ -1,50 +1,16 @@
 // 'use strict'
 
 import Pawn from '../pieces/pawn.js'
-import Rook  from '../pieces/rook.js'
-import Knight  from '../pieces/knight.js'
-import Bishop  from '../pieces/bishop.js'
-import Queen  from '../pieces/queen.js'
-import King  from '../pieces/king.js'
+import Rook from '../pieces/rook.js'
+import Knight from '../pieces/knight.js'
+import Bishop from '../pieces/bishop.js'
+import Queen from '../pieces/queen.js'
+import King from '../pieces/king.js'
 
-let wp00 = new Pawn([1,0],'white',startingBoard)
-let wp01 = new Pawn([1,1],'white',startingBoard)
-let wp02 = new Pawn([1,2],'white',startingBoard)
-let wp03 = new Pawn([1,3],'white',startingBoard)
-let wp04 = new Pawn([1,4],'white',startingBoard)
-let wp05 = new Pawn([1,5],'white',startingBoard)
-let wp06 = new Pawn([1,6],'white',startingBoard)
-let wp07 = new Pawn([1,7],'white',startingBoard)
-let bp00 = new Pawn([6,0],'black',startingBoard)
-let bp01 = new Pawn([6,1],'black',startingBoard)
-let bp02 = new Pawn([6,2],'black',startingBoard)
-let bp03 = new Pawn([6,3],'black',startingBoard)
-let bp04 = new Pawn([6,4],'black',startingBoard)
-let bp05 = new Pawn([6,5],'black',startingBoard)
-let bp06 = new Pawn([6,6],'black',startingBoard)
-let bp07 = new Pawn([6,7],'black',startingBoard)
- 
-
-let wr00 = new Rook()
-let wr01 = new Rook()
-let br00 = new Rook()
-let br01 = new Rook()
-
-let wk00 = new Knight()
-let wk01 = new Knight()
-let bk00 = new Knight()
-let bk01 = new Knight()
-
-let wb00 = new Bishop()
-let wb01 = new Bishop()
-let bb00 = new Bishop()
-let bb01 = new Bishop()
-
-let wque = new Queen()
-let bque = new Queen()
-
-let wkng = new King()
-let bkng = new King()
+let wr00, wk00, wb00, wque, wkng, wb01, wk01, wr01,
+    wp00, wp01, wp02, wp03, wp04, wp05, wp06, wp07,
+    bp00, bp01, bp02, bp03, bp04, bp05, bp06, bp07,
+    br00, bk00, bb00, bque, bkng, bb01, bk01, br01
 
 let startingBoard =
     [
@@ -58,7 +24,48 @@ let startingBoard =
         [br00, bk00, bb00, bque, bkng, bb01, bk01, br01],
     ];
 
-    export default null;
+
+wp00 = new Pawn([1, 0], 'white', startingBoard)
+wp01 = new Pawn([1, 1], 'white', startingBoard)
+wp02 = new Pawn([1, 2], 'white', startingBoard)
+wp03 = new Pawn([1, 3], 'white', startingBoard)
+wp04 = new Pawn([1, 4], 'white', startingBoard)
+wp05 = new Pawn([1, 5], 'white', startingBoard)
+wp06 = new Pawn([1, 6], 'white', startingBoard)
+wp07 = new Pawn([1, 7], 'white', startingBoard)
+bp00 = new Pawn([6, 0], 'black', startingBoard)
+bp01 = new Pawn([6, 1], 'black', startingBoard)
+bp02 = new Pawn([6, 2], 'black', startingBoard)
+bp03 = new Pawn([6, 3], 'black', startingBoard)
+bp04 = new Pawn([6, 4], 'black', startingBoard)
+bp05 = new Pawn([6, 5], 'black', startingBoard)
+bp06 = new Pawn([6, 6], 'black', startingBoard)
+bp07 = new Pawn([6, 7], 'black', startingBoard)
+
+
+wr00 = new Rook()
+wr01 = new Rook()
+br00 = new Rook()
+br01 = new Rook()
+
+wk00 = new Knight()
+wk01 = new Knight()
+bk00 = new Knight()
+bk01 = new Knight()
+
+wb00 = new Bishop()
+wb01 = new Bishop()
+bb00 = new Bishop()
+bb01 = new Bishop()
+
+wque = new Queen()
+bque = new Queen()
+
+wkng = new King()
+bkng = new King()
+
+
+export default null;
 export {
     startingBoard,
     wr00, wk00, wb00, wque, wkng, wb01, wk01, wr01,
