a8cdd52d0e777ab5cc1b206947ef7f49a209d678
AJ <alvianjoseph@gmail.com>
2019-10-31 17:47:32 -0700

    fixed bug where dragging pieces moved entire square, deleted corpse code, adds css styling

---
+++ src/components/board/game-board.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './board.css';
+import Spaces from './game-spaces.js';
+
+export default class Board extends React.Component {
+
+  renderSpace(i, j) {
+    let boardItems = this.props.board.startingBoard;
+    return <Spaces
+      board={boardItems}
+      pieceArr={this.props.board.pieceArr}
+      piece={boardItems[i][j]} //this gives access to all methods in Piece class
+      ycoord={i}
+      xcoord={j}
+    />
+  }
+
+  render() {
+    const board = [];
+    for (let i = 0; i < 8; i++) {
+      const rows = [];
+      for (let j = 0; j < 8; j++) {
+        rows.push(this.renderSpace(i, j));
+      }
+      board.push(<div className="board-row">{rows}</div>)
+    }
+
+    return (
+      <div className="board-container">
+        {board}
+      </div>
+    );
+  }
+}
