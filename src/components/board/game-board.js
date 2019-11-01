d7c728d46f206b58d439c48ddf669cd389147e4e
Brandyn Vay <brandynvay@protonmail.com>
2019-10-31 21:08:31 -0700

    added did mount to send board object to the api server

--- src/components/board/game-board.js
+++ src/components/board/game-board.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import './board.css';
-import Spaces from './game-spaces.js';
+import React from "react";
+import "./board.css";
+import Spaces from "./game-spaces.js";
+
+const API = "http://localhost:3000/api/v1/board";
 
 export default class Board extends React.Component {
+  componentDidMount() {
+    let board = this.props.board.startingBoard;
+    let json = JSON.stringify(board);
+    console.log(json);
+    console.log(board);
+    fetch(API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: json
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(console.error);
+  }
 
   renderSpace(i, j) {
     let boardItems = this.props.board.startingBoard;
-    return <Spaces
-      board={boardItems}
-      pieceArr={this.props.board.pieceArr}
-      piece={boardItems[i][j]} //this gives access to all methods in Piece class
-      ycoord={i}
-      xcoord={j}
-    />
+    return (
+      <Spaces
+        board={boardItems}
+        pieceArr={this.props.board.pieceArr}
+        piece={boardItems[i][j]} //this gives access to all methods in Piece class
+        ycoord={i}
+        xcoord={j}
+      />
+    );
   }
 
   render() {
@@ -22,13 +42,9 @@ export default class Board extends React.Component {
       for (let j = 0; j < 8; j++) {
         rows.push(this.renderSpace(i, j));
       }
-      board.push(<div className="board-row">{rows}</div>)
+      board.push(<div className="board-row">{rows}</div>);
     }
 
-    return (
-      <div className="board-container">
-        {board}
-      </div>
-    );
+    return <div className="board-container">{board}</div>;
   }
 }
