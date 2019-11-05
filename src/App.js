fdc82293c22c161ec7fecda7ed1eb70990e136d4
leeroywking <leeroywking@gmail.com>
2019-11-04 20:56:30 -0800

    homies

--- src/App.js
+++ src/App.js
@@ -4,79 +4,104 @@ import GameBoard from './components/board/game-board.js';
 import DisplayBoard from './components/board/display-board.js';
 import { checkChecker } from './board/boardmethods.js';
 import Updater from './api/board-updater.js';
+import DeadHomies from './components/board/deadHomies.js';
 
 // this imports the board as well as all the objects (pieces)
 // naming convention is in notes folder
 import * as boardItems from './board/startingBoard.js';
 
 function App() {
-  let defaultPieceState = { piece: null }
+  let defaultPieceState = { piece: null };
   let targetId;
 
   let [state, setState] = useState({ ...boardItems });
   let [activePiece, setActivePiece] = useState(defaultPieceState);
   let [moveList, setMoveList] = useState([]);
+  let [deadList, setDeadList] = useState({ white: [], black: [] });
 
   useEffect(() => {
-    const showAvailableMoves = (moves) => {
-
+    const showAvailableMoves = moves => {
       moves.forEach((move, idx) => {
-        targetId = `${move[0]}${move[1]}`
+        targetId = `${move[0]}${move[1]}`;
         let myId = document.getElementById(targetId);
         myId.classList.add('flash');
       });
     };
 
-    if (activePiece.piece) { showAvailableMoves(activePiece.piece.availableMoves) }
-
-  }, [activePiece])
-
-
+    if (activePiece.piece) {
+      showAvailableMoves(activePiece.piece.availableMoves);
+    }
+  }, [activePiece]);
 
   const returnToDefault = () => {
     let elems = document.querySelectorAll('span.flash');
     elems.forEach(elem => {
-      elem.classList.remove('flash')
-    })
-
+      elem.classList.remove('flash');
+    });
   };
 
   function handleClick(position) {
     if (activePiece.piece === null) {
-      setActivePiece({ piece: state.startingBoard[position[0]][position[1]] });
-    }
-    else {
-      let moveArr = [activePiece.piece.currentSpace , position];
-      setMoveList([...moveList, moveArr])
-      activePiece.piece.legalMove(position, state.startingBoard, state.pieceArr);
+      if (state.startingBoard[position[0]]) {
+        setActivePiece({
+          piece: state.startingBoard[position[0]][position[1]]
+        });
+      }
+    } else {
+      let moveArr = [activePiece.piece.currentSpace, position];
+      setMoveList([...moveList, moveArr]);
+      // grimReaper(position);
+
+      activePiece.piece.legalMove(
+        position,
+        state.startingBoard,
+        state.pieceArr
+      );
 
       setActivePiece(defaultPieceState);
       returnToDefault();
     }
-
   }
 
   function resetBoard() {
     let resettedBoard = state.resetBoard();
     setState({ ...state, startingBoard: resettedBoard });
   }
+  function grimReaper(position) {
+    console.log(position);
+    if (state.startingBoard[position[0]]) {
+      let victim = state.startingBoard[position[0]][position[1]];
+      console.log(victim);
+      if (victim) {
+        if (victim.color === 'white') {
+          deadList.white = [...deadList.white, victim];
+        }
+        if (victim.color === 'black') {
+          deadList.black = [...deadList.black, victim];
+        }
+        setDeadList({ deadList });
+      }
+    }
+  }
 
   return (
     <>
       <button onClick={() => resetBoard()}> Reset board</button>
+      <DeadHomies white={deadList.white} black={deadList.black} />
+      <Updater moves={moveList} />
       <DisplayBoard board={state} />
 
-      <div onClick={(e) => {
-        let clickedPiece = e.target.id.split('');
-        let parsedId = clickedPiece.map(num => {
-          return parseInt(num);
-        })
-        handleClick(parsedId);
-      }} >
-
+      <div
+        onClick={e => {
+          let clickedPiece = e.target.id.split('');
+          let parsedId = clickedPiece.map(num => {
+            return parseInt(num);
+          });
+          handleClick(parsedId);
+        }}
+      >
         <GameBoard board={state} />
       </div>
-      <Updater moves={moveList} />
     </>
   );
 }
---
+++ src/components/board/deadHomies.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function(props){
+    let deadWhite = props.white;
+    let deadBlack = props.black;
+    console.log(props)
+    return(
+        <>
+        <ul>
+            white dead pieces
+        {deadWhite.map( piece => {
+            return (<li> {piece.icon} </li>)
+        })}
+        </ul>
+        <ul>
+            black dead pieces
+        {deadBlack.map( piece => {
+            return (<li>{piece.icon}</li>)
+        })}
+        </ul>
+        </>
+    )
+}
