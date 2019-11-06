1b545be4f9a055c4547e9c353c9fa98e54e5e5a6
leeroywking <leeroywking@gmail.com>
2019-11-05 19:42:31 -0800

    Updates

---
+++ precache-manifest.3715ad051062839436fb1cb0721d1382.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b053619fd323ed79f7a37949c16352fe",
+    "url": "/Chess-react/index.html"
+  },
+  {
+    "revision": "dfa71e3f9f0315196613",
+    "url": "/Chess-react/static/css/main.2598f9a0.chunk.css"
+  },
+  {
+    "revision": "4d3ae5b5afcb160dbb5d",
+    "url": "/Chess-react/static/js/2.a1941a92.chunk.js"
+  },
+  {
+    "revision": "dfa71e3f9f0315196613",
+    "url": "/Chess-react/static/js/main.cb116c87.chunk.js"
+  },
+  {
+    "revision": "6b115bc196315249a50b",
+    "url": "/Chess-react/static/js/runtime-main.ac3e65e4.js"
+  },
+  {
+    "revision": "9dad278567a3603c7796f9926d1fdec2",
+    "url": "/Chess-react/static/media/bkn.9dad2785.png"
+  },
+  {
+    "revision": "3abe85b2d884d2ed62de975c99c93689",
+    "url": "/Chess-react/static/media/bq.3abe85b2.png"
+  },
+  {
+    "revision": "5fc256d06e01a55dbe8187f9dad1ee40",
+    "url": "/Chess-react/static/media/wb.5fc256d0.png"
+  },
+  {
+    "revision": "d4c3f8a77fc87c87a20de82e4282a8ea",
+    "url": "/Chess-react/static/media/wkn.d4c3f8a7.png"
+  },
+  {
+    "revision": "7e94829904b78b4c03641efbcabb4a1f",
+    "url": "/Chess-react/static/media/wp.7e948299.png"
+  },
+  {
+    "revision": "4192be626793237563bda45ac399c337",
+    "url": "/Chess-react/static/media/wq.4192be62.png"
+  }
+]);
