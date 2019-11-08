dc0a112f6e1b12410bcb6e314e258b243b67e744
AJ <alvianjoseph@gmail.com>
2019-11-07 19:48:15 -0800

    about us

--- src/components/about/about-us.js
+++ src/components/about/about-us.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import PuppyOne from '../../assets/puppy-one.jpg';
-import PuppyTwo from '../../assets/puppy-two.jpg';
-import PuppyThree from '../../assets/puppy-three.jpg';
-import PuppyFour from '../../assets/puppy-four.jpg';
+import BlackKing from '../../assets/chess-icons/bk.png';
+import WhitePawn from '../../assets/chess-icons/wp.png';
+import BlackKnight from '../../assets/chess-icons/bkn.png';
+import WhiteQueen from '../../assets/chess-icons/wq.png';
 import Nav from '../banner/banner.js';
 import './about-us.css';
 import '../../App.css';
@@ -18,22 +18,22 @@ export default function about() {
         <ul className="about">
           <li>
             <h1>Pratiibh</h1>
-            <img src={PuppyOne} width="300" />
+            <img src={BlackKnight} width="300" />
             <p>I like to code</p>
           </li>
           <li>
             <h1>Lee</h1>
-            <img src={PuppyTwo} width="300" />
+            <img src={WhiteQueen} width="300" />
             <p>I like to code</p>
           </li>
           <li>
             <h1>AJ</h1>
-            <img src={PuppyThree} width="300" />
-            <p>I like to code</p>
+            <img src={BlackKing} width="300" />
+            <p>After serving 6 years in the military as a Persian Farsi linguist and Intelligence analyst, I decided to apply those language learning and analytic skills to a career in coding. Incidentally, I enjoyed learning JavaScript so much I dropped everything and moved across the country to puruse my new goal.</p>
           </li>
           <li>
             <h1>Brandyn</h1>
-            <img src={PuppyFour} width="300" />
+            <img src={WhitePawn} width="300" />
             <p>I like to code</p>
           </li>
         </ul>
